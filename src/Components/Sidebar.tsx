8cae842049b9ffd571379c2349e478b58304d424
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2022-06-25 16:50:30 -0300

    update dates and conection with graphcms

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,18 +1,45 @@
+import { gql, useQuery } from '@apollo/client'
 import { Lesson } from './Lesson'
 
+const GET_LESSONS_QUERY = gql`
+  query MyQuery {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      availableAt
+      title
+      slug
+    }
+  }
+`
+interface GetLessonsQueryResponse {
+  lessons: {
+    id: string
+    lessontype: 'class' | 'live'
+    availableAt: string
+    title: string
+    slug: string
+  }[]
+}
 export function Sidebar() {
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
         Cronograma de aulas
       </span>
       <div className="flex flex-col gap-8">
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
+        {data?.lessons.map(lessons => {
+          return (
+            <Lesson
+              key={lessons.id}
+              title={lessons.title}
+              slug={lessons.slug}
+              availableAt={new Date(lessons.availableAt)}
+              type={lessons.lessontype}
+            />
+          )
+        })}
       </div>
     </aside>
   )
